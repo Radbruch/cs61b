964cbab1e7b3d5b56655147a290c658c89ada2bf
Radbruch <64899462+Radbruch@users.noreply.github.com>
2021-11-10 18:42:36 +0800

    push Lab 1 LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,4 +1,4 @@
-import com.sun.org.apache.xpath.internal.operations.And;
+//import com.sun.org.apache.xpath.internal.operations.And;
 
 /** Class that determines whether or not a year is a leap year.
  *  @author YOUR NAME HERE
