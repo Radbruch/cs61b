6bfc8935fb88e6007d121b18f2a8a1dfd1223277
Radbruch <64899462+Radbruch@users.noreply.github.com>
2021-12-18 22:38:39 +0800

    update proj1b

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,6 +1,6 @@
 
 public class OffByN implements CharacterComparator {
-    int n = 0;
+    private int n = 0;
 
     public OffByN(int N){
         n = N;
@@ -10,10 +10,7 @@ public class OffByN implements CharacterComparator {
     public boolean equalChars(char x, char y){
         int char1 = x;
         int char2 = y;
-        if (97 <= x && x <= 122 && 97 <= y && y <= 122) {
-            int diff = char1 - char2;
-            return diff == n || diff == -n;
-        }
-        throw new IllegalArgumentException("x and y must be lowercase letters");
+        int diff = char1 - char2;
+        return diff == n || diff == -n;
     }
 }
--- proj1b/OffByOne.java
+++ proj1b/OffByOne.java
@@ -5,10 +5,7 @@ public class OffByOne implements CharacterComparator{
     public boolean equalChars(char x, char y){
         int char1 = x;
         int char2 = y;
-        if (97 <= x && x <= 122 && 97 <= y && y <= 122) {
-            int diff = char1 - char2;
-            return diff == 1 || diff == -1;
-        }
-        throw new IllegalArgumentException("x and y must be lowercase letters");
+        int diff = char1 - char2;
+        return diff == 1 || diff == -1;
     }
 }
